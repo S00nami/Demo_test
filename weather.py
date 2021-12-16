47f383cc9aaccd9da8322713647520d18d44a49b
Jaden <96134288+S00nami@users.noreply.github.com>
2021-12-16 09:45:41 -0500

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,8 @@
+my_str= "Hello "
+name= input("What is your name?").capitalize()
+weather= input("What is the weather like today? ").lower()
+
+def greeting():
+    return f"{my_str} {name}, the weather forcast today is {weather}"
+
+print(greeting())
