63826b69c7feff9c03af86b75cc33fa73173db31
Jaden <96134288+htn-dev@users.noreply.github.com>
2022-01-01 18:10:06 -0500

    Create Happy-new-year.py

---
+++ Happy-new-year.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+#Initialise settings
+start=5
+message="🎈     Happy New Year!     🎈"
+
+#Start the countdown
+for counter in range(start,0,-1):
+    print(counter)
+    time.sleep(1)
+      
+
+#Display the message  
+print("v^v^v^v^v^v^v^v^v^v^v^v^v^v^v")
+print("<                           >")
+print(message)
+print("<                           >")
+print("v^v^v^v^v^v^v^v^v^v^v^v^v^v^v")
